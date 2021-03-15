1768af5d21957af47a9de55c1e683f67fcc372c4
cartman1978 <cartman_7802@yahoo.ie>
2021-03-15 19:46:45 +0000

    added onclick event to start_btn in order to show rules box if start btn is clicked

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// getting all the elements
+
+const start_btn = document.querySelector('.start_btn button');
+const info_container = document.querySelector('.info_container');
+
+
+// If start button is clicked
+start_btn.onclick = () => {
+    info_container.classList.add("activeInfo"); //show info box
+}
