7250724d297a415f856838559fdbc8e5c0c70576
cartman1978 <cartman_7802@yahoo.ie>
2021-03-17 11:35:00 +0000

    fixed bug, added transform translate because it was blocking start button

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,8 @@ const info_container = document.querySelector('.info_container');
 // If start button is clicked
 start_btn.onclick = () => {
     info_container.classList.add("activeInfo"); //show info box
-    console.log('button clicked');
+   
+    console.log('asdklfaserdihj');
     
 }
+
