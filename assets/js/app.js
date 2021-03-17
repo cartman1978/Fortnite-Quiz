68fca98fbafbd08e0f00f98c2ea8854bfb4fd9f5
cartman1978 <cartman_7802@yahoo.ie>
2021-03-17 10:26:11 +0000

    styled result-Box buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,6 @@ const info_container = document.querySelector('.info_container');
 // If start button is clicked
 start_btn.onclick = () => {
     info_container.classList.add("activeInfo"); //show info box
+    console.log('button clicked');
+    
 }
