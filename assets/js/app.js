a2b5208d030c80d8156f39c22471891fbbceef02
cartman1978 <cartman_7802@yahoo.ie>
2021-03-17 11:46:39 +0000

    added javascript functionality to start button quiz and exit the game

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,17 @@
 
 const start_btn = document.querySelector('.start_btn button');
 const info_container = document.querySelector('.info_container');
-
+const quit_btn = document.querySelector('.buttons .quit');
 
 // If start button is clicked
 start_btn.onclick = () => {
     info_container.classList.add("activeInfo"); //show info box
-   
-    console.log('asdklfaserdihj');
+
     
 }
 
+// If quit button is clicked
+
+quit_btn.onclick = () => {
+    info_container.classList.remove("activeInfo");
+}
