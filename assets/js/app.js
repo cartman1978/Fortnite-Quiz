d9489e0a1345d9129e45d19d3b78cce9aff06b8e
cartman1978 <cartman_7802@yahoo.ie>
2021-03-18 11:58:43 +0000

    added onclick function to continue btn  to show quiz question

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,9 @@
 
 const start_btn = document.querySelector('.start_btn button');
 const info_container = document.querySelector('.info_container');
-const quit_btn = document.querySelector('.buttons .quit');
-const continue_btn = document.querySelector('.buttons .restart');
-const quiz = document.querySelector('.quiz_box');
+const quit_btn = info_container.querySelector('.buttons .quit');
+const continue_btn = info_container.querySelector('.buttons .restart');
+const quiz_box = document.querySelector('.quiz_box');
 
 // If start button is clicked
 start_btn.onclick = () => {
@@ -23,5 +23,5 @@ quit_btn.onclick = () => {
 
 continue_btn.onclick = () => {
     info_container.classList.remove("activeInfo");
-    quiz.classList.add("quizActive");
+    quiz_box.classList.add("quizActive");
 }
