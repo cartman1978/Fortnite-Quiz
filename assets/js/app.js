6fb1c0fcc316f1f627a2105c1a363b7504593a0d
cartman1978 <cartman_7802@yahoo.ie>
2021-03-18 08:28:25 +0000

    added javascript functionality if continue btn is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@
 const start_btn = document.querySelector('.start_btn button');
 const info_container = document.querySelector('.info_container');
 const quit_btn = document.querySelector('.buttons .quit');
+const continue_btn = document.querySelector('.buttons .restart');
+const quiz = document.querySelector('.quiz_box');
 
 // If start button is clicked
 start_btn.onclick = () => {
@@ -15,4 +17,11 @@ start_btn.onclick = () => {
 
 quit_btn.onclick = () => {
     info_container.classList.remove("activeInfo");
+}
+
+// If continue btutton is clicked
+
+continue_btn.onclick = () => {
+    info_container.classList.remove("activeInfo");
+    quiz.classList.add("quizActive");
 }
