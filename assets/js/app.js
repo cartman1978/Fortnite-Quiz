d4808c011fd48e61339ff0848e3254f3cbf2f22c
cartman1978 <cartman_7802@yahoo.ie>
2021-03-28 12:39:23 +0000

    added function to fetch api

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,33 +62,19 @@ playGame.addEventListener('click', () => {
 });
 
 
-let questions = [
-    {
-        "question": "Inside which HTML element do we put the JavaScript??",
-        "choice1": "<script>",
-        "choice2": "<javascript>",
-        "choice3": "<js>",
-        "choice4": "<scripting>",
-        "answer": 1
-    },
-    {
-        "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        "choice1": "<script href='xxx.js'>",
-        "choice2": "<script name='xxx.js'>",
-        "choice3": "<script src='xxx.js'>",
-        "choice4": "<script file='xxx.js'>",
-        "answer": 3
-    },
-    {
-        "question": " How do you write 'Hello World' in an alert box?",
-        "choice1": "msgBox('Hello World');",
-        "choice2": "alertBox('Hello World');",
-        "choice3": "msg('Hello World');",
-        "choice4": "alert('Hello World');",
-        "answer": 4
-    }
-];
+let questions = [];
 
+fetch('https://opentdb.com/api.php?amount=15&category=15&difficulty=medium&type=multiple')
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        // questions = loadedQuestions;
+        console.log(loadedQuestions);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
 // Function to start the game
@@ -175,7 +161,7 @@ const validateUserAnswer = () => {
     });
 };
 
-startGame();
+// startGame();
 
 
 // Incremeant score if answer is correct
